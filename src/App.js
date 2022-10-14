fa4464dfc16981eb2a7e8fd0ebff459a145e283a
ionoffx <anes2354@gmail.com>
2022-10-14 17:50:22 +0900

    add module

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <Router>
+      <div className="App">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <button className="btn btn-primary">Button</button>
+      </div>
+    </Router>
   );
 }
 
