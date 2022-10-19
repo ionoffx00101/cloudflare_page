9c22442d7f40c1c80be587dc6c4cff04c9fb21d2
ionoffx <anes2354@gmail.com>
2022-10-19 17:03:06 +0900

    create notfound

--- src/components/Common/NotFoundPage.js
+++ src/components/Common/NotFoundPage.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { AlertTriangle } from "lucide-react";
 
 function NotFoundPage() {
   return (
     <div className="grid h-screen place-items-center">
-      존재하지 않는 페이지입니다.
+      <div className="mockup-window border bg-base-300">
+        <div className="flex flex-col justify-center px-28 py-28 bg-base-200">
+          <div className="grid place-items-center mb-4">
+            <AlertTriangle />
+          </div>
+          <div> 존재하지 않는 페이지입니다.</div>
+        </div>
+      </div>
     </div>
   );
 }
